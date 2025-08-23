4a441582895b7bcdfef02ca2635723a86797dccb
nathnael <134805961+nchanyal@users.noreply.github.com>
2025-08-23 13:00:30 -0400

    style: add parent component for form steps

---
+++ src/components/MultiStepForm/MultiStepForm.tsx
@@ -0,0 +1,9 @@
+export default function MultiStepForm() {
+  return (
+    <div className="min-h-screen min-w-screen flex justify-center items-center">
+      <div className="bg-white w-[1000px] h-[600px] max-w-full max-h-full rounded-2xl shadow-sm mx-4">
+        hello
+      </div>
+    </div>
+  );
+}
